140f3c98430cd6abe4b90fa820502e01ce846df0
zhoulixiang <zhoulixiang@kuaishou.com>
2024-11-03 01:50:28 +0800

    script: update zip script

--- scripts/zip.js
+++ scripts/zip.js
@@ -4,7 +4,7 @@ const { moveSync, removeSync } = require('fs-extra');
 
 async function main() {
   removeSync(path.resolve(__dirname, '../zip'));
-  moveSync(path.resolve(__dirname, '../zh/.vitepress/dist'), path.resolve(__dirname, '../zip/cn'));
+  moveSync(path.resolve(__dirname, '../zh/.vitepress/dist'), path.resolve(__dirname, '../zip/zh'));
   moveSync(path.resolve(__dirname, '../en/.vitepress/dist'), path.resolve(__dirname, '../zip/en'));
   await zip(path.resolve(__dirname, '../zip'), path.resolve(__dirname, '../dist.zip'));
   removeSync(path.resolve(__dirname, '../zip'));
